ce9ed1b062aaf695165608494f81574892e56767
PsPaul0719 <spencep0788@student.faytechcc.edu>
2025-09-02 14:21:03 -0400

    M1HW1_Spence.cpp
    Movie_Talk

---
+++ M1HW1_Spence.cpp
@@ -0,0 +1,24 @@
+// magic words
+// Movie Talk
+#include <iostream>
+using namespace std;
+
+int main() {
+
+string movie_name = "Everything Everywhere All at Once";
+
+string movie_date = "April 8th, 2022";
+
+string movie_gross = "$143 Million";
+
+string fav_quote = "'...But you, here, you're capable of anything because you're bad at everything.'";
+
+cout << "The movie " << movie_name << " was nationwide released on " << movie_date << ". And the movie grossed about" << movie_gross << ".";
+
+cout << "But numbers aside, the one thing I really love from this movie is this quote, " << fav_quote << ".";
+
+return 0;
+
+}
+
+
